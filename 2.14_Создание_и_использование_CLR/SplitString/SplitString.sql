de5e65bb76b2f13e76ffce63ecf67c3f2ff45b4e
Zaur Dzasezhev <dzasezhev@garant.ru>
2019-12-09 11:11:30 +0300

    Выполнено ДЗ-2.14 (простой вариант)

---
+++ 2.14_Создание_и_использование_CLR/SplitString/SplitString.sql
@@ -0,0 +1,26 @@
+﻿USE ClrDemo;
+
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+GO
+
+EXEC sp_configure 'clr enabled', 1;
+EXEC sp_configure 'clr strict security', 0;
+RECONFIGURE;
+GO
+
+--DROP ASSEMBLY ClrDemo 
+CREATE ASSEMBLY ClrDemo FROM 'C:\tmp\db\SplitString.dll' WITH PERMISSION_SET = SAFE;
+GO
+
+-- DROP FUNCTION SplitString
+CREATE FUNCTION dbo.StringSplit(@text nvarchar(max), @delimiter nchar(1))
+RETURNS TABLE (
+    part     nvarchar(max),
+    ID_ORDER int
+) WITH EXECUTE AS CALLER
+AS EXTERNAL NAME ClrDemo.UserDefinedFunctions.SplitString;
+GO 
+
+
+SELECT part FROM StringSplit('1;12;3;4;5;6;11;788;',';');
